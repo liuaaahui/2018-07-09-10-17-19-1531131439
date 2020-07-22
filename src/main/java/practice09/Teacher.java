ff06ae95101802a576295918880a50021f281fdb
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 19:27:15 +0800

    [Practice09][Penny]feature: added appendMember method in klass class and pass the test.

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,31 @@
 package practice09;
 
-public class Teacher {
+import practice09.Klass;
+import practice09.Student;
+
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this.klass == null) return super.introduce() + " I am a Teacher. I teach No Class.";
+        return super.introduce() + " I am a Teacher. I teach Class " + this.klass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber() == this.getKlass().getNumber()) return super.introduce() + " I am a Teacher. I teach " + student.getName() +".";
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+    }
 }
