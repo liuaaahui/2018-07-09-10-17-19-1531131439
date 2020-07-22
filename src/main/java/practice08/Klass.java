1911278abbf34c8dfd28cfe00b2758366f64bbfc
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 18:57:38 +0800

    [Practice08][Penny]feature: added id attribute in person class,added leader attribute in klass class and pass the test.

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,33 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        this.setLeader(student);
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,19 @@
 package practice08;
 
-public class Student {
+public class Student extends Person{
+    private practice08.Klass klass;
+
+    public Student(int id, String name, int age, practice08.Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this == klass.getLeader()) return super.introduce() + " I am a Student. I am Leader of Class " + this.klass.getNumber() + ".";
+        return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() + ".";
+    }
 }
