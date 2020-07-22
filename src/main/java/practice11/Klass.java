f4ba888169b01c0db50d44698538dc00ba42f94c
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 21:58:24 +0800

    [Practice11][Penny]feature: pass the test except for the last two tests.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,48 @@
 package practice11;
 
+import practice11.Student;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.number;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        this.setLeader(student);
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+
+    public boolean isIn(Klass klass){
+        if(klass.getNumber() == this.getNumber()) return true;
+        return false;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,50 @@
 package practice11;
 
-public class Teacher {
+import practice11.Klass;
+import practice11.Student;
+
+import java.util.List;
+
+public class Teacher extends Person{
+    private List<Klass> classes;
+    public Teacher(int id, String name, int age, List<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public List<Klass> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(List<Klass> classes) {
+        this.classes = classes;
+    }
+
+    public String introduce(){
+        if(this.classes == null) return super.introduce() + " I am a Teacher. I teach No Class.";
+        StringBuffer allClasses  = new StringBuffer();
+        classes.forEach(classesItem ->{
+            allClasses.append(classesItem.getNumber() + ", ");
+        });
+        return super.introduce() + " I am a Teacher. I teach Class " + allClasses.substring(0,allClasses.length()-2) + ".";
+    }
+
+    public String introduceWith(Student student){
+        if(isTeaching(student)) return super.introduce() + " I am a Teacher. I teach " + student.getName() +".";
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+    }
+
+    public boolean isTeaching(Student student){
+        boolean flag = false;
+        for(Klass classesItem:this.classes){
+            if(classesItem.isIn(student.getKlass())){
+                flag = true;
+            }
+        }
+        return flag;
+    }
 }
