39419f1bc5007183639b71339711e394bb917e68
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 17:39:01 +0800

    [Practice07][Penny]feature: added the klass class and pass the test.

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,28 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this.klass == null) return super.introduce() + " I am a Teacher. I teach No Class.";
+        return super.introduce() + " I am a Teacher. I teach Class " + this.klass.getNumber() + ".";
+    }
+
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber() == this.getKlass().getNumber()) return super.introduce() + " I am a Teacher. I teach " + student.getName() +".";
+        return super.introduce() + " I am a Teacher. I don't teach " + student.getName() +".";
+    }
 }
