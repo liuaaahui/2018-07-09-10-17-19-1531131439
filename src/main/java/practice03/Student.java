db32bbab3b191b79635d30ba070614c9d727211c
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 15:51:31 +0800

    [Practice03][Penny]feature: test the person class,the student class and the worker class about name ,age ,klass and introduce method.

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,22 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int Klass) {
+        super(name, age);
+        this.Klass = Klass;
+    }
+
+    public int getKlass() {
+        return Klass;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    public String introduce(){
+        return "I am a Student. I am at Class " + this.Klass + ".";
+    }
 }
