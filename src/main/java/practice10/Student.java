da747cbf2c8afb153fc6407b72bf1a9361355070
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-22 20:51:04 +0800

    [Practice10][Penny]feature: added isTeaching method and modify structure in teacher class and pass the test.

--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,25 @@
 package practice10;
 
-public class Student {
+import practice10.Klass;
+
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce(){
+        if(this == klass.getLeader()) return super.introduce() + " I am a Student. I am Leader of Class " + this.klass.getNumber() + ".";
+        return super.introduce() + " I am a Student. I am at Class " + this.klass.getNumber() + ".";
+    }
 }
