8b62696e208ca7147839b975531011a23c3645c3
PENNY LIU (ITA-ISDC-ISD-OOCLL/ZHA) <LIUPE8@OOCL.COM>
2020-07-26 11:30:48 +0800

    [Practice11][penny]refactor: fornat code

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -7,6 +7,7 @@ public class Klass {
     private final int number;
     private Student leader;
     private final List<JoinListener> joinListeners;
+
     public Klass(int number) {
         this.number = number;
         joinListeners = new ArrayList<>();
@@ -24,29 +25,29 @@ public class Klass {
         this.leader = leader;
     }
 
-    public String getDisplayName(){
+    public String getDisplayName() {
         return "Class " + this.number;
     }
 
-    public void assignLeader(Student student){
-        if(student.getKlass() != this){
+    public void assignLeader(Student student) {
+        if (student.getKlass() != this) {
             System.out.print("It is not one of us.\n");
             return;
         }
         this.setLeader(student);
-        for(JoinListener listener:this.joinListeners){
+        for (JoinListener listener : this.joinListeners) {
             listener.updateLeader(student);
         }
     }
 
-    public void appendMember(Student student){
+    public void appendMember(Student student) {
         student.setKlass(this);
-        for(JoinListener listener:this.joinListeners){
+        for (JoinListener listener : this.joinListeners) {
             listener.update(student);
         }
     }
 
-    public boolean isIn(Klass klass){
+    public boolean isIn(Klass klass) {
         return klass.getNumber() == this.getNumber();
     }
 
